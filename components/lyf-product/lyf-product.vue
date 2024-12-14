3a1aa0c30db2170ac0d3497e047a5aa0a35011c9
huaxie688 <mrli2902933052@gmail.com>
2024-12-14 22:18:53 +0800

    enhancement: 扩展分类基础组件 完成分类布局

--- components/lyf-product/lyf-product.vue
+++ components/lyf-product/lyf-product.vue
@@ -1,9 +1,24 @@
+
+<script setup>
+	
+	const props = defineProps({
+		categoryName: {
+			type: String,
+			default: 'Exclusive Offer'
+		}
+	})
+	const onSeeAllClick = () => {
+		console.log('Sell All')
+	}
+</script>
+
 <template>
 	<view class="product-wrapper">
-		<view class="pro-title">
-			<text class="left-text">Exclusive Offer</text>
-			<text class="right-text">See all</text>
+		<view class="pro-title" v-if="categoryName">
+			<text class="left-text">{{categoryName}}</text>
+			<text class="right-text" @click="onSeeAllClick">See all</text>
 		</view>
+		<slot name="extra"></slot>
 		<scroll-view class="pro-scroll-view" scroll-x="true">
 			<view class="scroll-view-items" v-for="(item,index) in 8" :key="index">
 				<lyf-product-item></lyf-product-item>
