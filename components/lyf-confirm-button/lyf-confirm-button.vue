5af413074c3d6b13d40e59d249493c727032b552
huaxie688 <mrli2902933052@gmail.com>
2025-01-01 12:35:50 +0800

    feat: 详情页布局完成

---
+++ components/lyf-confirm-button/lyf-confirm-button.vue
@@ -0,0 +1,33 @@
+<template>
+	<view class="confirm-button-wripper">
+		<button class="inner-button" :class="customButtonClass">{{buttonText}}</button>
+	</view>
+</template>
+
+<script setup>
+	const props = defineProps({
+		buttonText: {
+			type: String,
+			default: "Add To Basket"
+		},
+		customButtonClass: {
+			type: String,
+			default: ""
+		}
+	})
+</script>
+
+<style lang="scss" scoped>
+	.confirm-button-wripper {
+		padding: 0 54rpx;
+		.inner-button {
+			background-color: #53B175FF;
+			color: #FFF9FFFF;
+			font-size: 33rpx;
+			border: none;
+			border-radius: 30rpx;
+			height: 121rpx;
+			@include my-flex-set;	
+		}
+	}
+</style>
