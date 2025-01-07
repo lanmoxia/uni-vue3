e206d819eb6ea5c157df15d173e809935955768d
huaxie688 <mrli2902933052@gmail.com>
2025-01-07 21:49:03 +0800

    chore：搜索页面完成布局

--- pages/explore/explore.vue
+++ pages/explore/explore.vue
@@ -1,46 +1,78 @@
 <template>
-	<up-sticky bgColor="#fff" class="explore-card-sticky">
-		<view style="height:44px"></view>
-		<text class="explore-card-sticky-title">Find Products</text>
-		<view class="explore-card-sticky-bar">
-			<lyf-search-bar></lyf-search-bar>
-		</view>
-	</up-sticky>
-			
-	<scroll-view class="nav-scroll-view">
-		<view class="nav-card-item" v-for="(item,index) in 10" :key="index">
-			<lyf-search-nav-item></lyf-search-nav-item>
-		</view>
-	</scroll-view>
+	
+	<view class="explore-card">
+		<up-sticky bgColor="#fff" class="explore-card-sticky">
+			<view style="height: 45rpx"></view>
+			<text class="explore-card-sticky-title">Find Products</text>
+			<view class="explore-card-sticky-bar" :class="{'add-padding': isScroll}">
+				<lyf-search-bar :isFocus="isFocus"></lyf-search-bar>
+			</view>
+		</up-sticky>
+				
+		<scroll-view class="nav-scroll-view" scroll-y="true">
+			<view class="nav-card-item" v-for="(item,index) in 10" :key="index">
+				<lyf-search-nav-item></lyf-search-nav-item>
+			</view>
+		</scroll-view>
+	</view>
+
 </template>
 
 <script setup>
+	import {ref} from 'vue'
+	import {onShow} from "@dcloudio/uni-app"
 	
+	const isFocus = ref(false)
+	
+	const isScroll = ref(false)
+	
+	onShow(() => {
+		const pages = getCurrentPages() 
+		const currentPage = pages[pages.length - 1]
+		const query = currentPage.options
+		if(query.isFocus === 'true'){
+			isFocus.value = true
+		}
+	})
+	
+	const onScroll = () => {
+		console.log('hi')
+		
+	}
 </script>
 
 <style lang="scss" scoped>
-	.explore-card-sticky {
-		top: 0 !important;
-		padding: 0 47.2rpx;
-		&-title {
-			@include my-flex-set;
-			font-size: 36rpx;
-			color: #181725FF;					
+	
+	.explore-card {
+		height: 100vh;
+		.explore-card-sticky {
+			top: 0 !important;
+			padding: 0 47rpx;
+			.explore-nav-bar {
+			}
+			&-title {
+				@include my-flex-set;
+				font-size: 36rpx;
+				color: #181725FF;		
+			}
+			&-bar {
+				padding: 55rpx 0;
+				.add-padding {
+					padding-bottom: 55rpx;
+				}
+			}
 		}
-		&-bar {
-			margin-top: 55rpx;
-		}
-	}
-
-	.nav-scroll-view {
-		padding: 0 47rpx;
-		margin: 47rpx 0;
-		.nav-card-item {			
-			display: inline-block;
-			margin-bottom: 22rpx; 
-			margin-right: 22rpx; 
-			&:nth-child(2n) { margin-right: 0; } 
-			&:nth-last-child(-n+2) { margin-bottom: 0; }
+		
+		.nav-scroll-view {
+			padding: 0 47rpx calc(var(--window-bottom) + 47rpx) 47rpx;
+			height: 100%;
+			.nav-card-item {			
+				display: inline-block;
+				margin-bottom: 22rpx; 
+				margin-right: 22rpx; 
+				&:nth-child(2n) { margin-right: 0; } 
+				&:nth-last-child(-n+2) { margin-bottom: 0; }
+			}
 		}
 	}
 </style>
